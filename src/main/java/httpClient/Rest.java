cba76665b52d1ac079c1aff9894af5f5f01ffc5c
Naren <nvraizada@gmail.com>
2019-11-10 18:51:15 -0800

    API test file

--- src/main/java/httpClient/Rest.java
+++ src/main/java/httpClient/Rest.java
@@ -1,6 +1,5 @@
 package httpClient;
 
-import com.sun.xml.internal.bind.v2.TODO;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -22,38 +21,60 @@ public class Rest {
         return baseURI;
     }
 
-    public Response sendGET(String path, Map<String, String> headers, Map<String, String> queryParams){
+    public Response sendGET(String path) {
+        RestAssured.baseURI = baseURI;
+        return RestAssured.given().get(path).andReturn();
+    }
+
+    public Response sendGET(String path, Map<String, String> headers, Map<String, String> queryParams) {
         RestAssured.baseURI = baseURI;
         return RestAssured.given().headers(headers).queryParams(queryParams).get(path).andReturn();
     }
 
-    public Response sendGET(String path, Map<String, String> queryParams){
+    public Response sendGET(String path, Map<String, String> queryParams) {
         RestAssured.baseURI = baseURI;
         return RestAssured.given().queryParams(queryParams).get(path).andReturn();
     }
 
-    public Response sendPost(String path, Map<String, String> headers, Map<String, String> queryParams){
+    public Response sendPOST(String path, Map<String, String> headers, Map<String, String> queryParams) {
         RestAssured.baseURI = baseURI;
         return RestAssured.given().headers(headers).queryParams(queryParams).post(path).andReturn();
     }
 
-    public Response sendPost(String path, Map<String, String> headers, Map<String, String> queryParams, String body){
+    public Response sendPOST(String path) {
         RestAssured.baseURI = baseURI;
-        return RestAssured.given().headers(headers).queryParams(queryParams).body(body).post(path).andReturn();
+        return RestAssured.given().post(path).andReturn();
     }
 
-    public Response sendGET(String path, Map<String, String> headers, String body){
+    public Response sendPOST(String path, String body) {
+        RestAssured.baseURI = baseURI;
+        return RestAssured.given().body(body).post(path).andReturn();
+    }
+
+    public Response sendPOST(String path, String body, Map<String, String> queryParams) {
+        RestAssured.baseURI = baseURI;
+        return RestAssured.given().body(body).queryParams(queryParams).post(path).andReturn();
+    }
+
+    public Response sendPOST(String path, Map<String, String> headers, String body) {
         RestAssured.baseURI = baseURI;
         return RestAssured.given().headers(headers).body(body).post(path).andReturn();
     }
 
+    public Response sendDElETE(String path) {
+        RestAssured.baseURI = baseURI;
+        return RestAssured.given().delete(path).andReturn();
+    }
+
+    public  Response sendDELETE(String path, Map<String, String> headers){
+        RestAssured.baseURI = baseURI;
+        return RestAssured.given().headers(headers).delete(path).andReturn();
+    }
+
+    public Response sendDELETE(String path, String body, Map<String, String> headers){
+        RestAssured.baseURI=baseURI;
+        return RestAssured.given().body(body).headers(headers).delete(path).andReturn();
+    }
 
 
-    // TODO: 11/10/19
-
-    /*
-    1. POST with headers, query params
-    2. POST with headers, query params and String body
-    3. POST with headers and String body
-     */
 }
--- src/test/java/PetStoreAPI.java
+++ src/test/java/PetStoreAPI.java
@@ -1,12 +1,35 @@
 import httpClient.Rest;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PetStoreAPI {
 
-    private String baseURI = "";
-    private Rest rest = new Rest(baseURI);
+    private Rest rest = new Rest("https://petstore.swagger.io");
 
-    public void setup(){
-
+    @Test
+    public void getStoreInventory(){
+        Response response = rest.sendGET("/v2/store/inventory");
+        assert response.getStatusCode() == 200;
+    }
+    @Test
+    public void getStoreOrder(){
+        Response response = rest.sendGET("/v2/store/order/1");
+        assert response.getStatusCode() == 200;
+    }
+    @Test
+    public void deleteStoreOrder(){
+        Response response = rest.sendDElETE("v2/store/order");
+        assert response.getStatusCode() == 200;
+    }
+    @Test
+    public void postStoreOrder(){
+        Map<String,String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        Response response = rest.sendPOST("v2/store/order",headers,"{\"id\":0,\"petId\":0,\"quantity\":0,\"shipDate\":\"2019-11-11T02:09:58.111Z\",\"status\":\"placed\",\"complete\":true}");
+        assert  response.getStatusCode() == 200;
     }
 
 }
