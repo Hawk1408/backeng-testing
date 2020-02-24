2a5b43a88c6cc4e0c634f4285d80b3e1af97616f
Naren <nvraizada@gmail.com>
2020-02-23 17:42:00 -0800

    API test file

---
+++ src/test/java/SoapUI.java
@@ -0,0 +1,22 @@
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestCase;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestSuite;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+@Test
+public class SoapUI {
+    public void testTestCaseRunner() throws Exception
+    {
+        WsdlProject project = new WsdlProject( "/Users/Naren/Desktop/Project-1-soapui-project.xml" );
+        TestSuite testSuite = project.getTestSuiteByName( "Swagger Petstore TestSuite" );
+        TestCase testCase = testSuite.getTestCaseByName( "/user/login TestCase" );
+
+        // create empty properties and run synchronously
+        TestRunner runner = testCase.run( new PropertiesMap(), false );
+        assertEquals( TestRunner.Status.FINISHED, runner.getStatus() );
+    }
+}
